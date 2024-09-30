20b1697f0c366c6ed556cf557faa44a4301b8509
pushpak1497 <150467635+pushpak1497@users.noreply.github.com>
2024-09-30 12:51:51 +0530

    changes done to backend

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import express from "express";
 import userRouter from "./routes/userRoutes.js";
-import cors from "cors";
 
 const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
+app.use(express.static("build"));
 app.use(userRouter);
 
 export { app };
