5d5f540b5321496574c5f33f811c1519fea09b7e
pushpak1497 <150467635+pushpak1497@users.noreply.github.com>
2024-09-30 13:21:45 +0530

    address schema modified

--- src/models/addressModel.js
+++ src/models/addressModel.js
@@ -2,6 +2,6 @@ import mongoose, { Schema } from "mongoose";
 
 const addressSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  address: [{ type: String, required: true }],
+  address: [{ type: String, required: true, unique: true }],
 });
 export const Address = mongoose.model("Address", addressSchema);
